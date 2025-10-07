840b1b0f415c57a226a49476de152cfe87ff1fe0
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-10-07 14:21:53 -0300

    setup redux

--- help-desk/help-desk-backend/src/db/index.ts
+++ help-desk/help-desk-backend/src/db/index.ts
@@ -1,4 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { schema } from "./schema";
+import { eq } from "drizzle-orm";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
